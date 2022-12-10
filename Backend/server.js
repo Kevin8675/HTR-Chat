4791a134c7360ec543cd23bbde5209642e0de7d6
Kevin8675 <kevinkraze246@gmail.com>
2022-12-10 15:33:08 -0500

    trying to fix backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var http = require('http');
-var port = '8081'
-app.set('port', port);
+var port = '8081';
 var server = http.createServer(app);
-const io = require('socket.io')(port);
-io.on('connection',(socket)=>{
+const io = require('socket.io')(server);io.on('connection',(socket)=>{
   socket.on('join', function(data){
     socket.join(data.room);
     io.emit('new user joined', {user:data.user, message:'has joined  room.'});
--- src/app/chat.service.spec.ts
+++ src/app/chat.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ChatService } from './chat.service';
+import { chatService } from './chat.service';
 
 describe('ChatService', () => {
-  let service: ChatService;
+  let service: chatService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ChatService);
+    service = TestBed.inject(chatService);
   });
 
   it('should be created', () => {
